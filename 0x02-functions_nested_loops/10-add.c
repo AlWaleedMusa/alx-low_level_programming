3e0edb4db7dd42876c6bdefb233d30457e18a5a7
AlWaleed Musa <jako_249@hotmail.com>
2023-10-10 10:51:09 -0700

    Write a function that adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - is a function that add the first number to the secone.
+ * @x: the first parameter passed to the function.
+ * @y: the second parameter passed to the function.
+ * Return: the sum of the 2 parameters.
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
