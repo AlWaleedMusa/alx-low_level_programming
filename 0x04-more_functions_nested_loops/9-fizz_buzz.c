382b2bbed0eaf855c3a8dffa31375f6742ff32aa
AlWaleed Musa <jako_249@hotmail.com>
2023-10-12 14:08:32 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,11 +12,11 @@ int main(void)
 
 	while (i <= 100)
 	{
-		if ((i % 3) == 0)
-			printf("Fizz");
+		if ((i % 3) == 0 && (i % 5) == 0)
+			printf("FizzBuzz");
 		else if ((i % 5) == 0)
 			printf("Buzz");
-		else if ((i % 3) == 0 && (i % 5) == 0)
+		else if ((i % 3) == 0)
 			printf("FizzBuzz");
 		else
 			printf("%d", i);
