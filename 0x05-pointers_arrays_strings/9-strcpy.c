f416c096a22b9bc9aab8182de1237d720682cc6d
AlWaleed Musa <jako_249@hotmail.com>
2023-10-16 14:19:01 -0700

    Write a function that copies the string pointed to

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,11 +12,12 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	while (src[i])
+	do
 	{
-		dest[i] = src[i];
 		i++;
+		dest[i] = src[i];
 	}
+	while (src[i] != '\0');
 
 	return (dest);
 }
