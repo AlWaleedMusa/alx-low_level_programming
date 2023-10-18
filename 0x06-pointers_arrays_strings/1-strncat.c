1e39b66cb28fb4431143bd668b5be1aea534d72b
AlWaleed Musa <jako_249@hotmail.com>
2023-10-18 19:38:32 +0300

    bug fixed at task 1-strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,7 +4,7 @@
  * _strncat - a function that concatenates two strings using n.
  * @dest: first parameter passed to the function.
  * @src: second parameter passed to the function.
- * @n: third parameted passed to the function.
+ * @n: third parameter passed to the function.
  * Return: a pointer to a char.
  */
 
