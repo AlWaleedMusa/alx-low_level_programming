52ab8b1746e6a22605e9f3dfa61b6bbf55a7cab9
AlWaleed Musa <jako_249@hotmail.com>
2023-10-18 20:41:05 +0300

    general typos & bug fixed

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,16 +9,15 @@
 
 void reverse_array(int *a, int n)
 {
-	int i, j, temp;
+	int i, c;
 
-	j = n - 1;
 	i = 0;
 
-	while (i < n / 2)
+	while (i < (n - 1) / 2)
 	{
-		tmp = a[i];
-		a[i] = a[j];
-		a[j--] = tmp;
+		c = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = c;
 		i++;
 	}
 }
