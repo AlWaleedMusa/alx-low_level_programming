62148b76df6a8b833e0317f0d0887e2583d0d1ba
AlWaleed Musa <jako_249@hotmail.com>
2023-10-18 20:05:21 +0300

    bug fixed at 1-strcat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,23 +10,15 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int len1 = 0;
-	int len2 = 0;
-	int i = 0;
-	int size;
+	int index = strlen(dest);
+	int a = 0;
 
-	while (dest[len1] != '\0')
-		len1++;
-	while (src[len2] != '\0')
-		len2++;
-
-	size = len1 + len2 + 1;
-
-	while (i < n && src[i] != '\0')
+	while (a < n && *src)
 	{
-		dest[len1 + i] = src[i];
-		i++;
+		dest[index + a] = *src;
+		src++;
+		a++;
 	}
-
+	dest[index + a] = '\0';
 	return (dest);
 }
