76af6fa70fb7958eae78f5ece22d903199510c43
AlWaleed Musa <jako_249@hotmail.com>
2023-10-24 01:38:00 +0300

    bug fixes

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,21 +7,14 @@
 
 void print_chessboard(char (*a)[8])
 {
-	int i = 0;
-	int j = 0;
+	int i, j;
 
-	while (i < 8)
+	for (i = 0; i < 8; i++)
 	{
-		while (j < 8)
+		for (j = 0; j < 8; j++)
 		{
-			printf("%c ", a[i][j]);
-			if (j == 7)
-			{
-				printf("\n");
-			}
-			j++;
+			_putchar(a[i][j]);
 		}
-		j = 0;
-		i++;
+		_putchar('\n');
 	}
 }
