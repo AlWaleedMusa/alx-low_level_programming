9d027ba975008a2de8fee8ab0e1256029586cb59
AlWaleed Musa <jako_249@hotmail.com>
2023-10-23 14:16:12 +0300

    task 2-strchr - refactoring the code

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,19 +9,14 @@
 
 char *_strchr(char *s, char c)
 {
-	unsigned int i = 0;
-	char *cptr;
-
 	while (*s)
 	{
-		if (s[i] == c)
-		{
-			cptr = &s[i];
-			return (cptr);
-		}
-		else if (s[i] == '\0')
+		if (*s != c)
+			s++;
+		else
+			return (s);
+
+		if (*s == '\0')
 			return (NULL);
-		s++;
-		i++;
 	}
 }
