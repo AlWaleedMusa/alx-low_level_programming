367043b8711fd56128f689d28732dfabb5d955c0
AlWaleed Musa <jako_249@hotmail.com>
2023-10-25 23:18:51 +0300

    task 1-print-rev ready

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - print a string backwards.
+ * @s: the string passed to the function.
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+		_print_rev_recursion(s - 1);
+	printf("%c", *s);
+}
