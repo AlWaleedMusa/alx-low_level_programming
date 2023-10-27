4103f7997a46af333bf5d0defaa5c5d688fa86cb
AlWaleed Musa <jako_249@hotmail.com>
2023-10-27 19:44:54 +0200

    bug fixed

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,7 +7,7 @@
  * Return: 0 Always (Success).
 */
 
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc, __attribute__((__unused__))char *argv[])
 {
 	printf("%d\n", argc - 1);
 	
