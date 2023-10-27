861a98a77e565b4cc85642c6272a5b3540ecd1bd
AlWaleed Musa <jako_249@hotmail.com>
2023-10-27 08:13:22 +0200

    task 3-mul done

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point.
+ * Return: 0 Always (Success).
+ */
+
+int main(int argc, char *argv[])
+{
+	int mul = 1;
+	int i = 1;
+
+	while (i < argc)
+	{
+		if (argc > 2 && argc < 4)
+		{
+			mul = mul * atoi(argv[i]);
+			if (i == 2)
+				printf("%i\n", mul);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		i++;
+	}
+}
