9e2966f8abf8d24c82c30e01a9022426eefdfffc
AlWaleed Musa <jako_249@hotmail.com>
2023-10-30 19:08:37 +0200

    modified .gitignore/ 0x0b malloc file/ task 0-create

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * create_array - function that creates an array of chars.
+ * @size: the size of the array.
+ * @c: the char will be printed.
+ * Return: pointer to char.
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	int i = 0;
+
+	char *ptr = (char*)malloc(size * sizeof(c));
+
+	while (i < size)
+	{
+		ptr[i] = c;
+		i++;
+	}
+	return (ptr);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,10 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+
+#endif
