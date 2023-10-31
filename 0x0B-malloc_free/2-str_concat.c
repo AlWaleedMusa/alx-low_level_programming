bf547192da7de13904817a4023104eb72bea06a4
AlWaleed Musa <jako_249@hotmail.com>
2023-10-31 00:02:58 -0700

    2-strcat bug fixed

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,12 +20,10 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
 	while (s1[i] != '\0')
 		i++;
 	while (s2[j] != '\0')
 		j++;
-
 	size = i + j;
 
 	ptr = (char *)malloc(sizeof(char) * (size + 1));
@@ -48,6 +46,5 @@ char *str_concat(char *s1, char *s2)
 		}
 	}
 	ptr[size + 1] = '\0';
-
 	return (ptr);
 }
