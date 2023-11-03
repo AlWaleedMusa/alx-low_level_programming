e0aa2fffb8408b812e1e5f075c35695224d2fc7e
AlWaleed Musa <jako_249@hotmail.com>
2023-11-03 02:32:06 +0200

    2-calloc task done

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,11 +1,14 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#ifndef FILE_MAIN
-#define FILE_MAIN
+
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+char *_memset(char *s, char b, unsigned int n);
 
 #endif
