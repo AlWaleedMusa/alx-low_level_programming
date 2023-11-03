e49c5054bd6129945021d1d0ad82504d9641dbd5
AlWaleed Musa <jako_249@hotmail.com>
2023-11-03 02:26:38 +0200

    1-stringNconcat REFACTORING THE WHOLE CODE

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,54 +11,35 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int s1_len;
-	int s2_len;
-	int size;
-	int i = 0;
-	int j = 0;
-	char *ptr;
-	int num;
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	num = n;
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
 
-	if (num < 0)
+	if (!s)
 		return (NULL);
 
-	s1_len = strlen(s1);
-	s2_len = strlen(s2);
-
-	if (num >= s2_len && s2_len != 0)
+	while (i < len1)
 	{
-		num = s2_len;
-	}
-
-	size = num + s1_len;
-	ptr = (char *)malloc(sizeof(char) * size);
-
-	if (ptr == NULL)
-	{
-		return (NULL);
-	}
-
-	while (i < size)
-	{
-		ptr[i] = s1[i];
-		if (i == s1_len || s1_len == 0)
-		{
-			while (j < num)
-			{
-				ptr[i] = s2[j];
-				i++;
-				j++;
-			}
-		}
+		s[i] = s1[i];
 		i++;
 	}
-	ptr[size] = '\0';
-	return (ptr);
+
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
+
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
+
+	s[i] = '\0';
+
+	return (s);
 }
