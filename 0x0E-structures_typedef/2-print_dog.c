0280192c8663596d96e769b8738332484f37a4b9
AlWaleed Musa <jako_249@hotmail.com>
2023-11-06 20:00:14 +0200

    2-print_dog refactoring code

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,18 +1,22 @@
 #include "dog.h"
 
+/**
+ * print_dog - a function that print a struct.
+ * @d: struct passed to the function.
+ * Return: nothing
+*/
+
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	else
-	{
-		if (d->name == NULL)
-			printf("Name: (nil)\n");
-		else
-			printf("Name: %s\n", d->name);
-		if (d->owner == NULL)
-			printf("Owner: (nil)\n");
-		else
-			printf("Owner: %s\n", d->owner);
-	}
+	
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
 }
