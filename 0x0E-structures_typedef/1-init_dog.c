f8f61707d4f4acf8ecb479a6504564cf45530485
AlWaleed Musa <jako_249@hotmail.com>
2023-11-06 19:15:08 +0200

    1-inti_dog task done

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - a function that handel a struct dog.
+ * @d: pointer to a struct variable.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: owner of the dog.
+ * Return: nothing.
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
