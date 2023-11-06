b3ae1f11311c672d2d2aaa716938566a181733f4
AlWaleed Musa <jako_249@hotmail.com>
2023-11-06 20:06:46 +0200

    dog.h added a new struct using typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,12 +11,15 @@
  * Description: name dog's name, age is dog's age, owner is dog's owner.
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+/* typedef for struct dog*/
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
