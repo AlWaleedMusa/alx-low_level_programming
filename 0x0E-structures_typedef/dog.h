f4b36998fa82bb2c925325e6680e161d2c7502e5
AlWaleed Musa <jako_249@hotmail.com>
2023-11-06 19:08:45 +0200

    dog.h betty fixed (comments)

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - struct that define dog details.
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ * Description: name will be the name of the dog, age is the age of the dog in float numbers, owner is the name of the dog's owner
+ */
+
 struct dog
 {
 	char *name;
