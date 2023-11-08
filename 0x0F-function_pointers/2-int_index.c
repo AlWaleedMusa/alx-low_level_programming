4ceb860ac5426e527a6f96dc08c03cc76f18a8d6
AlWaleed Musa <jako_249@hotmail.com>
2023-11-08 15:22:52 +0200

    task 2-int_index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - search through an array.
+ * @array: array passed to the function.
+ * @size: size of the array.
+ * @cmp: pointer to a function.
+ * Return: int. 
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0)
+		return;
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
+	while (i < size)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+}
