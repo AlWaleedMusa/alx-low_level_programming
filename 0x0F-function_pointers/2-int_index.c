459f0a462bc5a0ccf84a63d16f6d342a0dde9953
AlWaleed Musa <jako_249@hotmail.com>
2023-11-08 15:35:50 +0200

    task 2-int_index fixed a compilation issue #3

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,13 +12,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (array == NULL || cmp == NULL)
-		return (-1);
-
-	while (i < size)
+	if (array && cmp)
 	{
-		if (cmp(array[i]) != 0)
-			return (i);
-		i++;
+		while (i < size)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+			i++;
+		}
 	}
+	return (-1);
 }
