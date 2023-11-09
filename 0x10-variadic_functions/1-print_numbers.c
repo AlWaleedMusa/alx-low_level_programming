f12b99d103b0878bbb996a1bb6bbfbf1350eacda
AlWaleed Musa <jako_249@hotmail.com>
2023-11-09 12:05:14 +0200

    task 1-print_numbers done

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - function that print all args passed.
+ * @separator: char that separate printed numbers.
+ * @n: number of args passed to the function.
+ * Return: nothing.
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i = 0;
+
+	va_start(args, n);
+	while (i < n)
+	{
+		printf("%d", va_arg(args, int));
+		printf("%s", separator);
+		i++;
+	}
+	va_end(args);
+}
