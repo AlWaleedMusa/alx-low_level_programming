1db43482a21c80f39221fb06e9e2c4148a3ab44f
AlWaleed Musa <jako_249@hotmail.com>
2023-11-09 11:38:26 +0200

    refactoring code/ fixed typo

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -6,6 +6,17 @@
  * Return: int.
  */
 
+int main(void)
+{
+    int sum;
+
+    sum = sum_them_all(2, 98, 1024);
+    printf("%d\n", sum);
+    sum = sum_them_all(4, 98, 1024, 402, -1024);
+    printf("%d\n", sum);    
+    return (0);
+}
+
 int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i = 0;
@@ -18,7 +29,7 @@ int sum_them_all(const unsigned int n, ...)
 
 	while (i < n)
 	{
-		sum += va_arg(sumOgAll, int);
+		sum += va_arg(sumOfAll, int);
 		i++;
 	}
 	va_end(sumOfAll);
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,6 +2,7 @@
 #define FILE_MAIN
 #include <unistd.h>
 #include <stdio.h>
+#include <stdarg.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
