e262cbb1e413844b2e42f283b39f5bb630162e8f
AlWaleed Musa <jako_249@hotmail.com>
2023-11-21 21:06:02 +0200

    task 7, second push

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,13 +9,19 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *ptr = head;
+	listint_t *ptr;
 	unsigned int i = 0;
+
+	ptr = head;
 
 	while (i < index)
 	{
 		ptr = ptr->next;
 		i++;
+	}
+	if (!ptr)
+	{
+		return (NULL);
 	}
 	return (ptr);
 }
