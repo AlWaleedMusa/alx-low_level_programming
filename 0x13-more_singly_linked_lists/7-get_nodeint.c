ab56ee8eb4c68b5bf68ce512674dd1cbde2d138a
AlWaleed Musa <jako_249@hotmail.com>
2023-11-21 21:10:06 +0200

    task 7, third push + betty function description

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,27 +1,29 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - 
+ * get_nodeint_at_index - look for a node by index.
  * @head: list passed to the function.
  * @index: index to get value from.
- * Return: a new node.
+ * Return: pointer to  node we want. or null
 */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *ptr;
+	listint_t *ptr = head;
 	unsigned int i = 0;
 
-	ptr = head;
-
-	while (i < index)
+	while (ptr && i < index)
 	{
 		ptr = ptr->next;
 		i++;
 	}
-	if (!ptr)
+
+	if (ptr)
+	{
+		return (ptr);
+	}
+	else
 	{
 		return (NULL);
 	}
-	return (ptr);
 }
