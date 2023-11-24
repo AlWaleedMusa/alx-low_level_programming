b681069b8f6917829d56a4c08753c60d6a408c5a
AlWaleed Musa <jako_249@hotmail.com>
2023-11-24 02:58:18 +0200

    last task done / first push

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns the number of bits you would need
+ * to flip to get from one number to another.
+ * @n: first int passed to use.
+ * @m: second int passed to use.
+*/
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int temp;
+	int c = 0;
+
+	temp = n ^ m;
+
+	while (temp)
+	{
+		c++;
+		temp &= (temp - 1);
+	}
+	return (c);
+}
