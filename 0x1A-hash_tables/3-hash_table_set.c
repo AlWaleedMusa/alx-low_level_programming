35b6e916b0c39ee9ea16acf3beb826f16d0e11b7
AlWaleed Musa <jako_249@hotmail.com>
2024-02-03 01:13:43 +0200

    task 3 DONE

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,55 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Add or update element in a hash table.
+ * @ht: A pointer to hash table.
+ * @key: The key to add.
+ * @value: The value associated with key.
+ * Return: Upon failure - 0. else 1.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *to_add;
+	char *value_copy;
+	unsigned long int idx, i;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	value_copy = strdup(value);
+
+	if (value_copy == NULL)
+		return (0);
+
+	idx = key_index((const unsigned char *)key, ht->size);
+
+	for (i = idx; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
+		}
+	}
+
+	to_add = malloc(sizeof(hash_node_t));
+
+	if (to_add == NULL)
+	{
+		free(value_copy);
+		return (0);
+	}
+	to_add->key = strdup(key);
+
+	if (to_add->key == NULL)
+	{
+		free(to_add);
+		return (0);
+	}
+	to_add->value = value_copy;
+	to_add->next = ht->array[idx];
+	ht->array[idx] = to_add;
+
+	return (1);
+}
